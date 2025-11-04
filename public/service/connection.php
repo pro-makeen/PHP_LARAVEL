85ad475827259db5f9fe42a93d2c308c1b954ed1
Pedro Rocha <pro.makeenenergy@gmail.com>
2025-11-04 16:43:31 +0000

    insert test

---
+++ public/service/connection.php
@@ -0,0 +1,8 @@
+<?php
+$host = "dpg-d3ua6jjipnbc738q7g5g-a.frankfurt-postgres.render.com"; 
+$port = "5432";      
+$dbname = "me_users";
+$user = "root";
+$password = "ybIeaygTzEWTg0AJnZ3T1yYZeTGtODUK";
+$dsn = "pgsql:host=dpg-d3ua6jjipnbc738q7g5g-a.frankfurt-postgres.render.com;port=5432;dbname=me_users;";
+?>
