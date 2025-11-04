326e33570079c695e11d64b4b25382c287b6b52c
Pedro Rocha <pro.makeenenergy@gmail.com>
2025-11-04 13:11:37 +0000

    Upadte simulation with php

--- public/service/php/simulation.php
+++ public/service/php/simulation.php
@@ -1,8 +1,27 @@
 <!DOCTYPE html>
+<html lang="en">
 <html>
+<head>
+    <meta charset="UTF-8">
+    <title>MAKEEN Energy - Simulator</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        #qr-reader {
+            width: 100%;
+            max-width: 600px;
+            margin: auto;
+        }
+        @media (max-width: 600px) {
+            #qr-reader {
+                width: 100%;
+            }
+        }
+    </style>
+</head>
+
 <body>
 
-<h1>Teste</h1>
+<h1>MAKEEN Energy Results</h1>
 
 <?php
 echo "Hello World!";
