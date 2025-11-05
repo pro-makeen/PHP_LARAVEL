0cd1257cf3430542021cf8b0e1242188d35c255a
Pedro Rocha <pro.makeenenergy@gmail.com>
2025-11-05 17:00:49 +0000

    2

--- public/service/php/simulation.php
+++ public/service/php/simulation.php
@@ -1,31 +1,15 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>MAKEEN Energy</title>
-</head>
-<body>
-    <p></p>
-    <p></p>
-    </body>
-</html>
-
 <?php
 
 $db_handle = pg_connect("host=dpg-d3ua6jjipnbc738q7g5g-a.frankfurt-postgres.render.com dbname=me_users user=root password=ybIeaygTzEWTg0AJnZ3T1yYZeTGtODUK");
 
 if ($db_handle) {
-
-
-
-
-    $result = pg_query($db_handle, "SELECT * FROM simulation_data");
+    
 
 echo 'Connection attempt succeeded.'; 
-echo $result;
+$query = "SELECT * FROM lpg_cylinder_basic_data";
+$result = pg_query($db_handle, $query);
 
 pg_close($db_handle);
-
-
 
 } else {
 
@@ -34,4 +18,42 @@ pg_close($db_handle);
 
 }
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Teste</title>
+</head>
+<body>
+    <h1>Teste</h1>
+    <table border="1" cellpadding="8">
+        <tr>
+            <th>ID</th>
+            <th>Create Date</th>
+            <th>Country</th>
+           
+        </tr>
+
+        <?php
+        if (pg_num_rows($result) > 0) {
+            while ($row = pg_fetch_assoc($result)) {
+                echo "<tr>
+                        <td>{$row['id']}</td>
+                        <td>{$row['create_adte']}</td>
+                        <td>{$row['country']}</td>
+                    </tr>"
+                    }
+        } else {
+            echo "<tr><td colspan='4'>No LPG cylinders found.</td></tr>";
+        }
+
+        pg_close($conn);
+        ?>               
+                    
+ </table>
+    <br>                 
+     </body>
+</html>
+
+
 
