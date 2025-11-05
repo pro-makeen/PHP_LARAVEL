41059a6cbd95619455d42affd8f44b77e28fcd1d
Pedro Rocha <pro.makeenenergy@gmail.com>
2025-11-05 17:09:28 +0000

    teste2

--- public/service/php/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = "dpg-d3ua6jjipnbc738q7g5g-a.frankfurt-postgres.render.com"; 
-$port = "5432";      
-$dbname = "me_users";
-$user = "root";
-$password = "ybIeaygTzEWTg0AJnZ3T1yYZeTGtODUK";
-//$dsn = "pgsql:host=dpg-d3ua6jjipnbc738q7g5g-a.frankfurt-postgres.render.com;port=5432;dbname=me_users;";
-$dsn = "pgsql:host=$host;port=$port;dbname=$dbname;";
-?>
--- public/service/php/simulation.php
+++ public/service/php/simulation.php
@@ -1,59 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>MAKEEN Energy</title>
+</head>
+<body>
+    <p></p>
+    <p></p>
+    </body>
+</html>
+
 <?php
 
 $db_handle = pg_connect("host=dpg-d3ua6jjipnbc738q7g5g-a.frankfurt-postgres.render.com dbname=me_users user=root password=ybIeaygTzEWTg0AJnZ3T1yYZeTGtODUK");
 
 if ($db_handle) {
-    
 
-echo 'Connection attempt succeeded.'; 
-$query = "SELECT * FROM lpg_cylinder_basic_data";
-$result = pg_query($db_handle, $query);
-
-pg_close($db_handle);
+echo 'Connection attempt succeeded.';
 
 } else {
 
 echo 'Connection attempt failed.';
-pg_close($db_handle);
 
 }
+
+// Get POST data
+$country = $_POST['country'];
+$production = $_POST['production'];
+$hours = $_POST['hours'];
+$gas_cylidner = $_POST['gas_cylinder'];
+$man_power = $_POST['man_power'];
+$service_team = $_POST['service_team'];
+$price_gas = $_POST['price_gas'];
+$profit_gas = $_POST['profit_gas'];
+$device = $_POST['device'];
+
+// Insert data securely
+$query = "INSERT INTO simulation_data (country, production, hours, gas_cylidner, man_power, service_team, price_gas, profit_gas, device) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)";
+$result = pg_query_params($db_handle, $query, array($country, $production, $hours; $gas_cylidner; $man_power, $service_team, $price_gas, $profit_gas, $device));
+
+if ($result) {
+    echo "✅ Simulation submitted.";
+
+    die() ;
+} else {
+ 
+    echo "❌ Error: " . pg_last_error($conn);
+   die() ;
+    
+}
+pg_close($db_handle);
+
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Teste</title>
-</head>
-<body>
-    <h1>Teste</h1>
-    <table border="1" cellpadding="8">
-        <tr>
-            <th>ID</th>
-            <th>Create Date</th>
-            <th>Country</th>
-           
-        </tr>
-
-        <?php
-        if (pg_num_rows($result) > 0) {
-            while ($row = pg_fetch_assoc($result)) {
-                echo "<tr>
-                        <td>{$row['id']}</td>
-                        <td>{$row['create_adte']}</td>
-                        <td>{$row['country']}</td>
-                    </tr>"
-                    }
-        } else {
-            echo "<tr><td colspan='4'>No LPG cylinders found.</td></tr>";
-        }
-
-        pg_close($conn);
-        ?>               
-                    
- </table>
-    <br>                 
-     </body>
-</html>
-
-
 
