1bdb3df7c95ae8a6190ec5b8533b66b0bbcf6d37
Pedro Rocha <pro.makeenenergy@gmail.com>
2025-11-05 08:58:19 +0000

    tsete

--- public/service/php/simulation.php
+++ public/service/php/simulation.php
@@ -1,27 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
 <html>
 <head>
-    <meta charset="UTF-8">
-    <title>MAKEEN Energy - Simulator</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <style>
-        #qr-reader {
-            width: 100%;
-            max-width: 600px;
-            margin: auto;
-        }
-        @media (max-width: 600px) {
-            #qr-reader {
-                width: 100%;
-            }
-        }
-    </style>
+    <title>LPG Cylinder Basic Data</title>
 </head>
-
 <body>
-
-<h1>MAKEEN Energy Results</h1>
+    <p></p>
+    <p></p>
+    </body>
+</html>
 
 <?php
 
@@ -63,5 +49,3 @@ pg_close($conn);
 
 ?>
 
-</body>
-</html>
--- public/service/php/test.php
+++ public/service/php/test.php
@@ -26,20 +26,6 @@
 <?php
 echo "PHP folder found!";
 
-$db_handle = pg_connect("host=dpg-d3ua6jjipnbc738q7g5g-a.frankfurt-postgres.render.com dbname=me_users user=root password=ybIeaygTzEWTg0AJnZ3T1yYZeTGtODUK");
-
-if ($db_handle) {
-
-echo 'Connection attempt succeeded.';
-
-} else {
-
-echo 'Connection attempt failed.';
-
-}
-
-pg_close($db_handle);
-
 ?> 
 
 </body>
