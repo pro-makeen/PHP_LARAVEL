362ca91c32291d1278386cad10dfeecefb49fe97
Pedro Rocha <pro.makeenenergy@gmail.com>
2025-11-06 15:57:57 +0000

    teste

--- public/service/php/simulation.php
+++ public/service/php/simulation.php
@@ -11,15 +11,6 @@
 
 <?php
 
-include __DIR__ . "../service/php/connection.php";
-
-// Connect to PostgreSQL
-$conn = pg_connect("host=$host dbname=$dbname user=$user password=$password");
-
-if (!$conn) {
-    die("Database connection failed: " . pg_last_error());
-}
-
 // Get POST data
 $country = $_POST['country'];
 $production = $_POST['production'];
@@ -30,21 +21,10 @@ $service_team = $_POST['service_team'];
 $price_gas = $_POST['price_gas'];
 $profit_gas = $_POST['profit_gas'];
 $device = $_POST['device'];
-
-// Insert data securely
-$query = "INSERT INTO simulation_data (country, production, hours, gas_cylidner, man_power, service_team, price_gas, profit_gas, device) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)";
-$result = pg_query_params($conn, $query, array($country, $production, $hours; $gas_cylidner; $man_power, $service_team, $price_gas, $profit_gas, $device));
-
-if ($result) {
-    echo "✅ Simulation submitted.<br><br>";
-
-    die() ;
-} else {
+namespace App\Http\Controllers;
  
-    echo "❌ Error: " . pg_last_error($conn);
-   die() ;
-    
-}
-pg_close($conn);
+use Illuminate\Support\Facades\DB;
+ 
+DB::insert('insert into me_users (country, production, hours, gas_cylidner, man_power, service_team, price_gas, profit_gas, device) values (?, ?, ?, ?, ?, ?, ?, ?, ?)', [$country, $production, $hours; $gas_cylidner; $man_power, $service_team, $price_gas, $profit_gas, $device]);
 
 ?>
